d931e20bada882a7d9b5064285e8fea439576880
vishal-kr-yadav <vkyadav9748@gmail.com>
2017-07-03 18:42:16 +0530

    First commit

---
+++ src/main/java/com/AWS/App.java
@@ -0,0 +1,13 @@
+package com.AWS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
